04cf6ee4aeaf83a827b96b77625490d33e23719c
Adejoke Osideko <osidekoadejoke@gmail.com>
2025-11-16 14:54:26 +0100

    Financial_inclusion_in_Africa.py, LR_model.pkl, scalr.pkl, 'Screenshot 2025-11-15 165813'

--- finacial.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-import pandas as pd
-import pickle
-with open("LR_model.pkl", "rb") as f:
-    LR_model= pickle.load(f)
-with open("LR_model.pkl", "rb")as f:
-    scalar = pickle.load(f)
